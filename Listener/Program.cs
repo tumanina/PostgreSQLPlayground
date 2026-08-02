b311f9317caca9c1c21bdcb3a2dd7a043865ba1b
HOME-PC\Olga <tumanina@yandex.ru>
2026-08-02 21:08:23 +0300

    Added listen/notify example

---
+++ Listener/Program.cs
@@ -0,0 +1,26 @@
+﻿using Npgsql;
+
+var connectionString = "Host=localhost;Port=5432;Database=playgrounddb;Username=postgres;Password=postgres";
+
+await using var connection = new NpgsqlConnection(connectionString);
+
+await connection.OpenAsync();
+
+connection.Notification += (_, e) =>
+{
+    Console.WriteLine($"Channel: {e.Channel}");
+    Console.WriteLine($"Payload: {e.Payload}");
+};
+
+await using var cmd = new NpgsqlCommand(
+    "LISTEN inbox;",
+    connection);
+
+await cmd.ExecuteNonQueryAsync();
+
+Console.WriteLine("Listening...");
+
+while (true)
+{
+    await connection.WaitAsync();
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,22 @@
+﻿using Npgsql;
+
+var connectionString = "Host=localhost;Port=5432;Database=playgrounddb;Username=postgres;Password=postgres";
+await using var connection = new NpgsqlConnection(connectionString);
+
+await connection.OpenAsync();
+
+while (true)
+{
+    var message = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(message))
+        continue;
+
+    await using var cmd = new NpgsqlCommand(
+        "SELECT pg_notify('inbox', @message);",
+        connection);
+
+    cmd.Parameters.AddWithValue("message", message);
+
+    await cmd.ExecuteNonQueryAsync();
+}
